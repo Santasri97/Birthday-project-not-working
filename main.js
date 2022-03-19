25a105cb784b8a228c841eb966a1dc18f4221ae6
Santasri97 <90270303+Santasri97@users.noreply.github.com>
2022-03-19 17:05:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ block_image_object = Img;
 block_image_object.scaleToWidth(700);
 block_image_object.scaleToHeight(510);
 block_image_object.set({
-    top = 0,
-    left = 0
+    top : 0,
+    left : 0
 });
 canvas.add(block_image_object);
 });	
